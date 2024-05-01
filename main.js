d4f479e5c639bc01ef6008281f628405f6000a9e
Abdurehman99 <159618780+Abdurehman99@users.noreply.github.com>
2024-05-01 08:02:33 +0500

    Word Count

---
+++ main.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let userAns = await inquirer.prompt({
+    name: "words",
+    type: "input",
+    message: "Please enter yoursentence",
+});
+let wordCount = userAns.words.trim().split(" ").length;
+console.log(`your sentence has ${wordCount}`);
